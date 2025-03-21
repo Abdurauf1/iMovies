9b028be4a5bf820ab4aaa40af4cb990c03ad8701
Abdurauf <aabdurauf004@gmail.com>
2025-03-22 02:24:23 +0500

    refactor

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { movieApi } from "../features/MovieApiSlice";
 import { animeApi } from "../features/AnimeApiSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -11,5 +12,8 @@ export const store = configureStore({
     getDefaultMiddleware().concat(movieApi.middleware, animeApi.middleware)
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
