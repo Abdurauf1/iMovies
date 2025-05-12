f75c83f97ce7f638a3bcb143f42e4d885942c219
Abdurauf <aabdurauf004@gmail.com>
2025-05-12 19:47:31 +0500

    update

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,20 +1,18 @@
 import { FC } from "react"
-import { useParams } from "react-router-dom"
-import { useGetShowQuery } from "../features/GetShowsApiSlice"
+// import { useParams } from "react-router-dom"
+// import { useGetShowQuery } from "../features/GetShowsApiSlice"
 
 const Detail: FC = () => {
-  const { category, id } = useParams()
+  // const { category, id } = useParams()
 
-  const { data, isLoading, isError } = useGetShowQuery({
-    category: String(category),
-    id: Number(id)
-  })
-
-  const { title } = data.movies;
+  // const { data, isLoading, isError } = useGetShowQuery({
+  //   category: String(category),
+  //   id: Number(id)
+  // })
 
   return (
     <>
-      {title}
+      Detail
     </>
   )
 }
